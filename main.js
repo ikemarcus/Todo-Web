1c9d754daf3ae6001628fdb81dc80b2b43a22316
ikemarcus <i.marcus@sintlucasedu.nl>
2022-05-13 12:20:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -53,3 +53,11 @@ function newElement() {
     }
   }
 }
+
+var input = document.getElementById("myInput");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("myBtn").click();
+  }
+});
